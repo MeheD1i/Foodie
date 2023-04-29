fe22077896a1ef582a2bc381aea36b48ef4301bd
Mehedi Hasan <kpmh222@gmail.com>
2023-04-29 18:03:41 +0600

    signup with validation added

--- mernapp/src/App.js
+++ mernapp/src/App.js
@@ -9,7 +9,7 @@ import Login from './screens/Login';
 import '../node_modules/bootstrap-dark-5/dist/css/bootstrap-dark.min.css'  //npm i bootstrap-dark-5 bootstrap
 import '../node_modules/bootstrap/dist/js/bootstrap.bundle';
 import "../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js";
-
+import Signup from './screens/Signup';
 function App() {
   return (
     <Router>
@@ -17,6 +17,7 @@ function App() {
         <Routes>
           <Route exact path="/" element={<Home />} />
           <Route exact path="/login" element={<Login />} />
+          <Route exact path="/creatuser" element={<Signup />} />
         </Routes>
       </div>
     </Router>
