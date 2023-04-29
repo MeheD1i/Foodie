a371c8d537a5a293a8e42dc9a704862fe639392e
Mehedi Hasan <kpmh222@gmail.com>
2023-04-29 13:50:31 +0600

    fixed fetch error

--- mernapp/backend/db.js
+++ mernapp/backend/db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const mongoURI = 'mongodb://foodie:w2oWm1qXMANd3h4g@ac-fwgvaot-shard-00-00.dsjpcwo.mongodb.net:27017,ac-fwgvaot-shard-00-01.dsjpcwo.mongodb.net:27017,ac-fwgvaot-shard-00-02.dsjpcwo.mongodb.net:27017/?ssl=true&replicaSet=atlas-i9e37g-shard-0&authSource=admin&retryWrites=true&w=majority'
+const mongoURI = 'mongodb://foodie:w2oWm1qXMANd3h4g@ac-fwgvaot-shard-00-00.dsjpcwo.mongodb.net:27017,ac-fwgvaot-shard-00-01.dsjpcwo.mongodb.net:27017,ac-fwgvaot-shard-00-02.dsjpcwo.mongodb.net:27017/foodiemern?ssl=true&replicaSet=atlas-i9e37g-shard-0&authSource=admin&retryWrites=true&w=majority'
 const mongoDB = async () => {
     await mongoose.connect(mongoURI, { useNewUrlParser: true }, async (err, result) => {
         if (err) console.log("---", err)
