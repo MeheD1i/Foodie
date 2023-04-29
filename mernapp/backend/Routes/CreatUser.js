6c54d435324a020245cc2384510aa36c7f3b8dc2
Mehedi Hasan <kpmh222@gmail.com>
2023-04-29 15:35:43 +0600

    express validation added

--- mernapp/backend/Routes/CreatUser.js
+++ mernapp/backend/Routes/CreatUser.js
@@ -1,14 +1,25 @@
 const express = require('express')
 const router = express.Router()
 const User = require('../models/User')
+const { body, validationResult } = require('express-validator');
 
-router.post("/creatuser", async (req, res) => {
+router.post("/creatuser", [
+    body('email').isEmail(),
+    body('name').isLength({ min : 5}),
+    body('password','Incorrect Password').isLength({ min : 5})]
+,async (req, res) => {
+
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
+
     try {
         await User.create({
-            name: "Mehedi Hasan",
-            password: "123456",
-            email: "mhasan12@hotmail.com",
-            location: "Dhaka"
+            name: req.body.name,
+            password: req.body.password,
+            email: req.body.email,
+            location: req.body.location
         })
         res.json({ success: true });
 
