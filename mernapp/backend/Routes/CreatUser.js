6d802ffec6dd2e16226add364f7e5bb2a1ea21a6
Mehedi Hasan <kpmh222@gmail.com>
2023-05-23 16:29:34 +0600

    jwt_token_added

--- mernapp/backend/Routes/CreatUser.js
+++ mernapp/backend/Routes/CreatUser.js
@@ -2,8 +2,9 @@ const express = require('express')
 const router = express.Router()
 const User = require('../models/User')
 const { body, validationResult } = require('express-validator');
-
+const jwt = require("jsonwebtoken");
 const bcrypt = require("bcryptjs");
+const jwtSecret = "PjSFOwXHqVcTEyDUazmBKLixNnGpfl$#"
 
 router.post("/creatuser", [
     body('email').isEmail(),
@@ -48,11 +49,20 @@ router.post("/loginuser", [
             if (!userData) {
                 return res.status(400).json({ errors: "Try logging in with correct credentials" })
             }
-            if (req.body.password !== userData.password) {
+           
+            const pwdCompare = await bcrypt.compare(req.body.password,userData.password) //  comparing password
+            if (!pwdCompare) {
                 return res.status(400).json({ errors: "Try logging in with correct credentials" })
             }
 
-            return res.json({ success: true })
+            const data = {
+                user:{
+                    id:userData.id
+                }
+            }
+
+            const authToken = jwt.sign(data,jwtSecret)
+            return res.json({ success: true,authToken:authToken })
         } catch (error) {
             console.log(error)
             res.json({ success: false });
--- mernapp/src/screens/Login.js
+++ mernapp/src/screens/Login.js
@@ -21,6 +21,8 @@ export default function Login() {
       alert("Enter Valid Credentials")
     }
     if (json.success) {
+      localStorage.setItem("authToken",json.authToken);
+      console.log(localStorage.getItem("authToken"))
       navigate("/");
     }
   }
