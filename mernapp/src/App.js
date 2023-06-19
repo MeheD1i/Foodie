f6103716c6887a7d65da28145ef369e97905de86
Mehedi Hasan <kpmh222@gmail.com>
2023-06-20 04:27:37 +0600

    order page added

--- mernapp/src/App.js
+++ mernapp/src/App.js
@@ -12,6 +12,7 @@ import "../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js";
 import Signup from './screens/Signup';
 import { CartProvider } from './components/ContextReducer';
 import '@fortawesome/fontawesome-free/css/all.min.css';
+import MyOrder from './screens/MyOrder';
 
 
 function App() {
@@ -23,6 +24,7 @@ function App() {
           <Route exact path="/" element={<Home />} />
           <Route exact path="/login" element={<Login />} />
           <Route exact path="/creatuser" element={<Signup />} />
+          <Route exact path="/myOrder" element={<MyOrder />} />
         </Routes>
       </div>
     </Router>
